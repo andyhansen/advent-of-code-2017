8c06a10a119120ec61df964fc59d28c3f93bd72f
andyhansen <ahansen.nz@gmail.com>
2017-12-03 19:39:45 +1300

    skeleton for part 2

---
+++ 02/day2-part2.exs
@@ -0,0 +1,29 @@
+# defmodule CaptchaSolver do
+#     def get_value(file, accumulator) do
+#     end
+
+#     def get_value(file, accumulator) do
+#         accumulator
+#     end
+# end
+
+
+defmodule LineMagic do
+  def subtract_max_from_min(line) do
+    Enum.max(line) - Enum.min(line)
+  end
+
+  def find_matching_and_divide(line) do
+  end
+end
+
+file = File.read! "02/input.txt"
+
+file = 
+  String.split(file, "\n")
+    |> Enum.map(&(String.split(&1, "\t")))
+    |> Enum.map(fn line -> (Enum.map(line, &(String.to_integer(&1)))) end)
+    |> Enum.map(fn line -> LineMagic.find_matching_and_divide(line) end)
+    |> Enum.sum
+
+IO.puts file
