91d18a0aed00d91f3f66d07504a8704c3c43285c
andyhansen <ahansen.nz@gmail.com>
2017-12-03 19:36:20 +1300

    cleaner looking part 1

--- 02/day2-part1.exs
+++ 02/day2-part1.exs
@@ -7,17 +7,20 @@
 #     end
 # end
 
-file = File.read! "02/input.txt"
 
-def subtract_max_from_min(line)
-  Enum.max(line) - Enum.min(line)
+defmodule LineMagic do
+  def subtract_max_from_min(line) do
+    Enum.max(line) - Enum.min(line)
+  end
 end
+
+file = File.read! "02/input.txt"
 
 file = 
   String.split(file, "\n")
     |> Enum.map(&(String.split(&1, "\t")))
     |> Enum.map(fn line -> (Enum.map(line, &(String.to_integer(&1)))) end)
-    |> Enum.map(fn line -> Enum.max(line) - Enum.min(line) end)
+    |> Enum.map(fn line -> LineMagic.subtract_max_from_min(line) end)
     |> Enum.sum
 
 IO.puts file
